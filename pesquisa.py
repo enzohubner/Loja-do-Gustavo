500060f528ecd32ad8d0025b10cd6262b02db9be
Enzo Hubner <94123023+enzohubner@users.noreply.github.com>
2024-11-01 19:06:35 -0300

    adição da pesquisa.py

---
+++ pesquisa.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+conn = psycopg2.connect('')
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM usuarios")
+
+rows = cursor.fetchall()
+print(rows)
+for row in rows:
+    column1 = row[0]
+    column2 = row[1]
+    column3 = row[2]
+    column4 = row[3]
+    print(column1, column2, column3, column4)
+
+conn.commit()  
+cursor.close()
+conn.close()

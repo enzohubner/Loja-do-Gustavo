bd3343d4c194f596f2f24e3f8260e14c8585a986
Enzo Hubner <94123023+enzohubner@users.noreply.github.com>
2024-11-01 19:05:52 -0300

    Adicao do app.py

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 from flask import Flask, request, render_template, redirect
-
+import psycopg2
+from psycopg2 import sql
 
 app = Flask(__name__)
+
+def bd():
+    return 'CREATE TABLE usuarios (id SERIAL PRIMARY KEY,nome VARCHAR(100),email VARCHAR(100) UNIQUE,senha VARCHAR(100))'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Obter os dados do formulário
         nome = request.form['nome']
         email = request.form['email']
         senha = request.form['senha']
         
-        return render_template('resposta.html', nome = nome, email = email, senha = senha)
+        conn = psycopg2.connect('')
+        cursor = conn.cursor()
+        
+        inserir_query = "INSERT INTO usuarios (nome, email, senha) VALUES (%s, %s, %s)"
+        cursor.execute(inserir_query, (nome, email, senha))
+
+        conn.commit()  
+        cursor.close()
+        conn.close()
+
+        return render_template('resposta.html', nome=nome, email=email, senha=senha)
     else:
         return render_template('index.html')
 
 @app.route('/resposta', methods=['GET'])
-def resposta(nome,email,senha):
-    return render_template('resposta.html', nome = nome, email = email, senha = senha)
-    
+def resposta():
+    nome = request.args.get('nome')
+    email = request.args.get('email')
+    senha = request.args.get('senha')
+    return render_template('resposta.html', nome=nome, email=email, senha=senha)
 
 if __name__ == '__main__':
     app.run(debug=True)

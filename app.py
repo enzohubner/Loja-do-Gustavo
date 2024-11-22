1c02d4a5e1ba78256687aaa44808466f0c4ac8c4
RafaelPerovano <rafaperovano@gmail.com>
2024-11-22 19:25:00 -0300

    crud de produtos

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from psycopg2 import sql
 from db import cursor, conn
 app = Flask(__name__)
 
-def bd():
-    return 'CREATE TABLE usuarios (id SERIAL PRIMARY KEY,nome VARCHAR(100),email VARCHAR(100) UNIQUE,senha VARCHAR(100))'
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -47,6 +44,117 @@ def login():
         return jsonify({"message":"Campos incompletos"}), 400
     else:
         return render_template('login.html')
+
+@app.route('/altera_usuario', methods=['GET', 'POST'])
+def altera_usuario():
+    if request.method == 'POST':
+        email_antigo = request.form['email_antigo']
+        nome = request.form['nome']
+        email_novo = request.form['email']
+        senha = request.form['senha']
+
+        if nome and email_antigo and email_novo and senha:
+            cursor.execute("UPDATE usuarios SET nome = %s, email = %s, senha = %s WHERE email = %s",(nome, email_novo, senha, email_antigo))
+            conn.commit()
+            return redirect('/lista_usuario')
+        else:
+            return jsonify({"message": "Campos incompletos"}), 400
+    else:
+        email = request.args.get('email') 
+        if not email:
+            return jsonify({"message": "Email não fornecido"}), 400
+
+        cursor.execute("SELECT nome, email FROM usuarios WHERE email = %s", (email,))
+        usuario = cursor.fetchone()
+
+        if not usuario:
+            return jsonify({"message": "Usuário não encontrado"}), 404
+
+        return render_template('altera_usuario.html', usuario=usuario)
+
+
+@app.route('/deleta_usuario', methods=['POST'])
+def deleta_usuario():
+    if request.method == 'POST':
+        email = request.form['email']
+
+        if email:
+            cursor.execute("DELETE FROM usuarios WHERE email = %s", (email,))
+            conn.commit()
+
+            return redirect('/lista_usuario')  
+        else:
+            return jsonify({"message": "Campos incompletos"}), 400
+    else:
+        return redirect('/deleta_usuario') 
+
+
+@app.route('/cadastra_produto', methods=['GET', 'POST'])
+def cadastra_produto():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        preco = request.form['preco']
+        quantidade = request.form['quantidade']
+
+        if nome and descricao and preco and quantidade: 
+            cursor.execute("INSERT INTO produtos (nome, descricao, preco, quantidade) VALUES (%s, %s, %s, %s)",(nome, descricao, preco, quantidade))
+            conn.commit()
+
+            return redirect('/produtos')
+        else:
+            return jsonify({"message": "Campos incompletos"}), 400
+
+    return render_template('cadastra_produto.html')
+
+
+@app.route('/produtos', methods=['GET'])
+def lista_produtos():
+    cursor.execute("SELECT id, nome, descricao, preco, quantidade FROM produtos")
+    produtos = cursor.fetchall()
+
+    return render_template('produtos.html', produtos=produtos)
+
+@app.route('/edita_produto', methods=['GET', 'POST'])
+def edita_produto():
+    if request.method == 'POST':
+        nome = request.form['nome']  
+        novo_nome = request.form['novo_nome'] 
+        descricao = request.form['descricao']
+        preco = request.form['preco']
+        quantidade = request.form['quantidade']
+
+        if nome and novo_nome and descricao and preco and quantidade: 
+            cursor.execute("UPDATE produtos SET nome=%s, descricao=%s, preco=%s, quantidade=%s WHERE nome=%s", (novo_nome, descricao, preco, quantidade, nome))
+            conn.commit()
+            return redirect('/produtos')
+        else:
+            return jsonify({"message": "Campos incompletos"}), 400 
+
+    else:
+        nome = request.args.get('nome')  
+        if not nome:
+            return jsonify({"message": "Nome não fornecido"}), 400
+
+        cursor.execute("SELECT nome, descricao, preco, quantidade FROM produtos WHERE nome=%s", (nome,))
+        produto = cursor.fetchone()
+
+        if not produto:  
+            return jsonify({"message": "Produto não encontrado"}), 404
+
+        return render_template('editar_produto.html', produto=produto)
+
+@app.route('/deleta_produto', methods=['POST'])
+def deleta_produto():
+    nome = request.form['nome']  
+
+    if nome:
+        cursor.execute("DELETE FROM produtos WHERE nome = %s", (nome,))
+        conn.commit()
+        return redirect('/produtos')
+    else:
+        return jsonify({"message": "Nome do produto não fornecido"}), 400  
+
 
 @app.route('/navbar', methods=['GET'])
 def altera():

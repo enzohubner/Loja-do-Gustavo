4d19219c60f08444f687fe9c0a6e11575363d638
Enzo Hubenr <enzonsei@gmail.com>
2024-11-25 14:59:38 -0300

    Ajuste para os requisitos de entrega

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
 
         return redirect('/login')
     else:
-        return render_template('home.html')
+        return render_template('index.html')
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -101,7 +101,7 @@ def cadastra_produto():
             cursor.execute("INSERT INTO produtos (nome, descricao, preco, quantidade) VALUES (%s, %s, %s, %s)",(nome, descricao, preco, quantidade))
             conn.commit()
 
-            return redirect('/produtos')
+            return redirect('/menu')
         else:
             return jsonify({"message": "Campos incompletos"}), 400
 
@@ -166,10 +166,6 @@ def altera():
 def teste():
     return render_template('cadastra_produto.html')
 
-@app.route('/altera_usuario', methods=['GET', 'POST'])
-def altera_usuario():
-    return render_template('altera_usuario.html')
-
 @app.route('/notificacoes', methods=['GET', 'POST'])
 def notificacoes():
     return render_template('notificacoes.html')
@@ -177,6 +173,25 @@ def notificacoes():
 @app.route('/editar_produto', methods=['GET', 'POST'])
 def editar_produtos():
     return render_template('editar_produto.html')
+
+@app.route('/menu', methods=['GET', 'POST'])
+def menu():
+    produtos = [
+        {"id": 1, "nome": "Produto A", "descricao": "Descrição do Produto A", "preco": 10.99, "quantidade": 100},
+        {"id": 2, "nome": "Produto B", "descricao": "Descrição do Produto B", "preco": 20.99, "quantidade": 200},
+        {"id": 3, "nome": "Produto C", "descricao": "Descrição do Produto C", "preco": 30.99, "quantidade": 300},
+        {"id": 4, "nome": "Produto D", "descricao": "Descrição do Produto D", "preco": 40.99, "quantidade": 400},
+        {"id": 5, "nome": "Produto E", "descricao": "Descrição do Produto E", "preco": 50.99, "quantidade": 500}
+    ]
+    return render_template('menu.html', produtos=produtos)
+
+@app.route('/alt_usuario', methods=['GET', 'POST'])
+def alt_usuario():
+    return render_template('altera_usuario.html')
+
+@app.route('/contato', methods=['GET', 'POST'])
+def contato():
+    return render_template('contato.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
